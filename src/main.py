5bd68ad68fa802a2a538d6ea61c6cc930fdf28e3
Daniel Ipince <ipince22@gmail.com>
2021-03-20 23:58:29 +0000

    v1.0

--- src/main.py
+++ src/main.py
@@ -10,6 +10,11 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
 #from models import Person
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import datetime
+#from secret import super_secret
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,6 +24,52 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
+# Create a route to authenticate your users and return JWTs. The
+@app.route('/login', methods=["POST"])
+def login():
+    if request.method == "POST":
+        username = request.json["username"]
+        password = request.json["password"]
+
+        # Validate
+        if not username:
+            return jsonify({"error": "username Invalid"}), 400
+        if not password:
+            return jsonify({"error": "Password Invalid"}), 400
+        
+        user = User.query.filter_by(username=username).first()
+
+        if not user:
+            return jsonify({"error": "User not found"}), 400
+        
+        #if not check_password_hash(user.password, password):
+        #    return jsonify({"error": "Wrong password"}), 400
+        
+        # Create Access Token
+        expiration_date = datetime.timedelta(days=3)
+        access_token = create_access_token(identity=username, expires_delta=expiration_date)
+
+        request_body = {
+            "user": user.serialize(),
+            "token": access_token
+        }
+
+        return jsonify(request_body), 200
+
+
+
+@app.route('/user', methods=["GET"])
+@jwt_required()
+def lista_usuarios():
+    current_user = get_jwt_identity()
+    users = User.query.all()
+    request_body = list(map(lambda user:user.serialize(),users))
+    return jsonify(request_body),200
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
